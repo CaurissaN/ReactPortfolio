3d8521dfd0098fcee3c0244bbe080b0147bc0184
CaurissaN <caurissanev@gmail.com>
2023-07-20 16:37:09 -0400

    setting up initial layout

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+
+
+
+  return (
+    <header>
+      <h1>Caurissa Nevy</h1>
+      <nav>
+        <li >About</li>
+        <li >Portfolio</li>
+        <li >Resume</li>
+        <li >Contact</li>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
