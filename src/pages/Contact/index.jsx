e164e2cd9ab518fc963c8780d8b54d2c00ffdb63
CaurissaN <caurissanev@gmail.com>
2023-07-28 16:29:52 -0400

    updated portfolio page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+  const [state, handleSubmit] = useForm("mdoraovk");
+  if (state.succeeded) {
+    return <p>Thanks for contacting me!</p>;
+  }
+
   return (
     <div>
       <h1>Contact Me</h1>
+    <form className='contactForm' onSubmit={handleSubmit}>
+       <input
+        id="name"
+        type="name"
+        name="name"
+        placeholder='Name...'
+      />
+      <ValidationError
+        prefix="Name"
+        field="name"
+        errors={state.errors}
+      />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder='Email...'
+      />
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+        placeholder='Message...'
+      />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
     </div>
   )
-}
+};
 
-export default Contact
+export default Contact;
--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import projectData from "../../portfolio.json"
 
 function Portfolio() {
   return (
-    <div>
+    <div className='portfolio'>
       <h1>My Portfolio</h1>
+      <section>
+        {projectData.map((proj) => {
+          return (
+          <div key={proj.id} className='card'>
+            <img className='projImg' alt={proj.name} src={proj.image} />
+            <h1>{proj.title}</h1>
+            <div>
+              <a target='blank' href={proj.github}>
+              <button>GitHub</button>
+              </a>
+              <a target='blank' href={proj.live}>
+              <button>Live</button>
+              </a>
+            </div>
+          </div>
+          )
+        })}
+      </section>
     </div>
   )
 }
